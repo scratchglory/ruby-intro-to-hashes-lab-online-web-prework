f1d7144d6531538388c5753e5db70ae987dce742
Adella <adellakenn@gmail.com>
2019-05-03 13:25:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,11 +18,9 @@ def monopoly_with_second_tier
 	  {
 	  :pieces => 4,
     :names => {},
-    :rent_in_dollars => 
-      {:one_piece_owned => 25}
+    :rent_in_dollars => {}
 	  }
-	  
-	}
+	 }
 end
 
 def monopoly_with_third_tier
